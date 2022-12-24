122960beb7ed4406ace74bc30b45e3d28052e88a
KijkEr <joep_is_cool@hotmail.com>
2022-12-24 15:00:24 +0100

    Ophalen attributes toegevoegd

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,17 @@ pub struct Weapon {
     pub damage_type: String,
     pub quality: String,
 }
+pub struct Attributes {
+    pub strength: i16,
+    pub dexterity: i16,
+    pub constitution: i16,
+    pub intelligence: i16,
+    pub wisdom: i16,
+    pub charisma: i16,
+}
 pub struct PlayerCharacter {
     pub character: Character,
+    pub attributes: Attributes,
     pub weapon: Weapon,
 }
 
@@ -51,11 +60,21 @@ impl DBApplication {
         transaction.commit().await?;
         Ok(weapon)
     }
+    async fn get_attributes(&self) -> Result<Attributes, sqlx::Error> {
+        let mut transaction = self.pool.begin().await?;
+        let attributes = sqlx::query_file_as!(Attributes, "./queries/attributes.sql")
+            .fetch_one(&mut transaction)
+            .await?;
+        transaction.commit().await?;
+        Ok(attributes)
+    }
     pub async fn build_character(&self) -> PlayerCharacter {
         let player = self.get_character().await;
         let weapon = self.get_weapon().await;
+        let attributes = self.get_attributes().await;
         PlayerCharacter {
             character: player.unwrap(),
+            attributes: attributes.unwrap(),
             weapon: weapon.unwrap(),
         }
     }
