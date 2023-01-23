ecea15d288361cb262d7959175ad13f807ae9780
KijkEr <joep_is_cool@hotmail.com>
2023-01-23 20:21:48 +0100

    Nested JSON gepruts

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -3,13 +3,40 @@ import data from "./data/character.json";
 
 function App() {
   const getHeadings = () => {
-    return Object.keys(data[0]);
+    return Object.keys(data[0].character[0]);
   };
+
+  console.log(getHeadings());
+  console.log(data[0].character);
 
   return (
     <div className="container">
-      <Table theadData={getHeadings()} tbodyData={data} />
+      <Table theadData={getHeadings()} tbodyData={data[0].character} />
     </div>
   );
 }
 export default App;
+
+// import Table from "./character/character";
+// import data from "./data/character.json";
+
+// function App() {
+//   const getCharacter = () => {
+//     return data[0].character;
+//   };
+
+//   console.log(getCharacter());
+
+//   const getHeadings = () => {
+//     return Object.keys(data[0].character);
+//   };
+
+//   console.log(getHeadings());
+
+//   return (
+//     <div className="container">
+//       <Table theadData={getHeadings()} tbodyData={getCharacter()} />
+//     </div>
+//   );
+// }
+// export default App;
